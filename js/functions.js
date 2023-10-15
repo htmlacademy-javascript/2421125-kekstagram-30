fe3154c36c672f97d8c1449feb5026296ad8e6ed
Igor Gordeev <igorgo01@mail.ru>
2023-10-15 12:32:10 +0300

    Исправление замечаний - 2

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,17 @@
 // Функция для проверки длины строки
 
 
-// eslint-disable-next-line no-unused-vars
 function strLength(str, characters) {
   return str.length <= characters;
 }
-
+strLength();
 
 // Функция для проверки длины строки
 
-// eslint-disable-next-line no-unused-vars
 function isPalindrome(str) {
   str = str.toString();
 
   str = str.toLowerCase().replace(/\s|[,.!?"'/-]/g, '');
   return str === str.split('').reverse().join('');
 }
+isPalindrome();
