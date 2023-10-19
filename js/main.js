d90431fda4d70288eee5c2890d57e9d0b9b4c062
Igor Gordeev <igorgo01@mail.ru>
2023-10-19 22:28:39 +0300

    Решение Пункт4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,78 @@
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !',
+];
 
+const NAMES = [
+  'Анна',
+  'Максим',
+  'Екатерина',
+  'Алексей',
+  'София',
+  'Иван',
+  'Виктория',
+];
+
+const PHOTO_DESCRIPTIONS = [
+  'Закат на пляже',
+  'Романтическая встреча',
+  'Городские огоньки',
+  'Путешествие в горы',
+  'Цветущие сады',
+  'Вспышка',
+];
+
+// Функция генерирующая числа
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Функция генерирующая ID
+
+function generateId() {
+  let id = 0;
+  return function () {
+    return id++;
+  };
+}
+
+const generatePhotoId = generateId();
+const generateCommentId = generateId();
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+// Функция, возвращающая комменатрий в виде объекта
+
+const createComment = () => {
+  const message = getRandomArrayElement(MESSAGES);
+  const commentId = generateCommentId();
+  return {
+    id: commentId,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: message.trim(),
+    name: getRandomArrayElement(NAMES),
+  };
+};
+
+// Функция, возвращающая фотографию со значениями в виде объекта
+const createElement = () => {
+  const photoId = generatePhotoId();
+  return {
+    id: photoId,
+    url: `img/${getRandomInteger(1, 25)}.svg`,
+    likes: getRandomInteger(15, 200),
+    description: getRandomArrayElement(PHOTO_DESCRIPTIONS),
+    comments: Array.from({ length: getRandomInteger(0, 30) }, createComment)
+  };
+};
+
+Array.from({ length: 25 }, createElement);
