d9a73f22e8e4ae3c509fba0ee490df6a079519f4
Igor Gordeev <igorgo01@mail.ru>
2023-10-29 16:58:30 +0300

    5.16 - задание

--- js/functions.js
+++ js/functions.js
@@ -15,3 +15,18 @@ function isPalindrome(str) {
   return str === str.split('').reverse().join('');
 }
 isPalindrome();
+
+const timeToString = (timeString) => {
+  const [hours, minutes] = timeString.split(':');
+  return hours * 60 + Number(minutes);
+};
+
+const isWorkingTime = (startWork, endWork, startMeeting, longMeeting) => {
+  startWork = timeToString(startWork);
+  endWork = timeToString(endWork);
+  startMeeting = timeToString(startMeeting);
+  return startMeeting >= startWork && startMeeting + longMeeting <= endWork;
+};
+
+isWorkingTime('08:00', '18:00', '10:00', 120);
+isWorkingTime('08:00', '17:00', '16:30', 90);
